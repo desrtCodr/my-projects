032ea980b61436f948be9d1d77d94f2501e95f20
desrtCodr <ian.mitchard@gmail.com>
2023-03-29 14:29:46 -0600

    updated api file extension

--- src/pages/api/api.tsx
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-type type = {
-  name: string;
-};
-const url = 'https://www.boredapi.com/api/activity';
-
-export default function fetchActivity(type: type) {
-  return fetch(`${url}?type=${type}`).then((res) => res.json());
-}
