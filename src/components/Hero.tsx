94738105f037a26b7181fec7dcb96c5e115de3c3
desrtCodr <ian.mitchard@gmail.com>
2023-03-28 18:55:52 -0600

    tailwind files

---
+++ src/components/Hero.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+type HeroProps = {
+  heading: string;
+  about: string;
+  projects: string;
+};
+
+const Hero = ({ heading, about, projects }: HeroProps) => {
+  return (
+    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
+      {/* Overlay */}
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
+      <div className='p-5 text-white z-[2] mt-[-10rem]'>
+        <h2 className='text-5xl font-bold break-word'>{heading}</h2>
+        <p className='py-5 text-xl'>{about}</p>
+        <button className='py-3 text-xl border p-4'>
+          {projects}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,96 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+const path = [
+  { uid: 2, name: 'Projects', id: 3, path: 'projects' },
+  { uid: 3, name: 'About', id: 2, path: 'about' },
+  { uid: 4, name: 'Contact', id: 3, path: 'contact' },
+];
+
+function Navbar() {
+  const [nav, setNav] = React.useState(false);
+  const [color, setColor] = React.useState('bg-transparent');
+  const [textColor, setTextColor] = React.useState('text-white');
+
+  const handleClick = () => {
+    setNav(!nav);
+  };
+  React.useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 20) {
+        setColor('bg-white');
+        setTextColor('black');
+      } else {
+        setColor('bg-transparent');
+        setTextColor('white');
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`${color} fixed left-0 top-0 w-full z-10 ease-in duration-300`}
+    >
+      <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
+        <Link href='/'>
+          <h1
+            style={{ color: `${textColor}` }}
+            className='font-bold text-2xl hover:text-gray-500'
+          >{`<ianMitchard.com />`}</h1>
+        </Link>
+        <ul className='hidden sm:flex'>
+          {path.map((value) => {
+            return (
+              <li
+                key={value.uid}
+                className={`${textColor} p-4 hover:text-gray-500`}
+              >
+                <Link href={value.path}>{value.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        {/* Mobile Button */}
+        <div onClick={handleClick} className='block sm:hidden z-10'>
+          {nav ? (
+            <AiOutlineClose
+              size={20}
+              style={{ color: `${textColor}` }}
+            />
+          ) : (
+            <AiOutlineMenu
+              size={20}
+              style={{ color: `${textColor}` }}
+            />
+          )}
+        </div>
+        <div
+          className={
+            nav
+              ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+              : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+          }
+        >
+          <ul>
+            {path.map((value) => {
+              return (
+                <li
+                  key={value.uid}
+                  className='p-4 text-2xl hover:text-gray-500'
+                >
+                  <Link href={value.path}>{value.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
