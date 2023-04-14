005349d0a596de94d259235c169b1f107216e75f
desrtCodr <ian.mitchard@gmail.com>
2023-04-14 14:51:53 -0600

    update projects names

--- src/data/sliderPicsData.js
+++ src/data/sliderPicsData.js
@@ -5,18 +5,18 @@ export const sliderPicsData = [
   },
   {
     src: 'https://images.unsplash.com/photo-1593453727617-f7959b3e8917?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    alt: 'hiking',
+    alt: 'project 2',
   },
   {
     src: 'https://images.unsplash.com/photo-1610412327313-645b21903718?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    alt: 'climbing',
+    alt: 'project 3',
   },
   {
     src: 'https://images.unsplash.com/photo-1592427527147-5d2541620df1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-    alt: 'mountain biking',
+    alt: 'project 4',
   },
   {
     src: 'https://images.unsplash.com/photo-1635786155668-4fabf1131e57?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    alt: 'paragliding',
+    alt: 'project 5',
   },
 ];
