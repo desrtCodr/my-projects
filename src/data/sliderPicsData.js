7c75235fce2f416bcecdf77f41d7675d889466de
desrtCodr <ian.mitchard@gmail.com>
2023-05-10 13:09:04 -0600

    task manager photo

--- src/data/sliderPicsData.js
+++ src/data/sliderPicsData.js
@@ -10,7 +10,7 @@ export const sliderPicsData = [
     path: '/notBored',
   },
   {
-    src: 'https://images.unsplash.com/photo-1610412327313-645b21903718?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    src: 'https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80',
     alt: 'Task Manager App',
     path: 'https://task-manager-alpha-seven.vercel.app/',
   },
